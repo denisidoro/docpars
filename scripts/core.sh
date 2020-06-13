060bcc2db31d8d07dc27b648670e75a1e1a00dc3
Denis Isidoro <denis.isidoro@uber.com>
2020-06-13 19:35:40 -0300

    wip

--- scripts/core.sh
+++ scripts/core.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
 
+DOTFILES_COMMIT_HASH="bc74f8"
+
 dot::install_if_necessary() {
     [ -n "${DOTFILES:-}" ] && return
     export DOTFILES="${DOCPARS_HOME}/dotfiles"
     export PATH="${DOTFILES}/bin:${PATH}"
     git clone "https://github.com/denisidoro/dotfiles.git" "$DOTFILES"
     cd "$DOTFILES"
-    git checkout "bc74f8"
+    git checkout "$DOTFILES_COMMIT_HASH"
+}
+
+parse::python() {
+   local -r msg="$1"
+   shift
+   "${DOTFILES}/scripts/core/docopts" -h "$msg" : "$@"
+}
+
+parse::docpars() {
+   local -r msg="$1"
+   shift
+   "${DOCPARS_HOME}/scripts/run" -h "$msg" : "$@"
+}
+
+parse::compare() {
+   local -r docopt_output="$(parse::python "$@" | sort)"
+   local -r docpars_output="$(parse::docpars "$@" | sort)"
+   echo "$docpars_output" | test::equals "$docopt_output"
+}
+
+fixture::get() {
+   cat "${DOCPARS_HOME}/tests/docs/${1}.txt"
 }
 
 export DOT_DOCOPT="${DOT_DOCOPT:-"${DOCPARS_HOME}/target/debug/docpars"}"
--- tests/dotfiles.sh
+++ tests/dotfiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source "${DOCPARS_HOME}/tests/aux/core.sh"
+source "${DOCPARS_HOME}/scripts/core.sh"
 
 export DOT_DOCOPT="${DOCPARS_HOME}/target/debug/docpars"
 source "${DOTFILES}/tests/help.sh"
