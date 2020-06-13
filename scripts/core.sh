84f13a06ac86e7d67003d0637b8c3ee949d53450
Denis Isidoro <denis.isidoro@uber.com>
2020-06-13 19:44:56 -0300

    wip

--- scripts/core.sh
+++ scripts/core.sh
@@ -7,8 +7,7 @@ dot::install_if_necessary() {
     export DOTFILES="${DOCPARS_HOME}/dotfiles"
     export PATH="${DOTFILES}/bin:${PATH}"
     git clone "https://github.com/denisidoro/dotfiles.git" "$DOTFILES"
-    cd "$DOTFILES"
-    git checkout "$DOTFILES_COMMIT_HASH" 2>/dev/null || true
+    $(cd "$DOTFILES" && git checkout "$DOTFILES_COMMIT_HASH" 2>/dev/null || true)
 }
 
 parse::python() {
@@ -33,6 +32,6 @@ fixture::get() {
    cat "${DOCPARS_HOME}/tests/docs/${1}.txt"
 }
 
-echo "DOCPARS_HOME: $DOCPARS_HOME"
+export DOCPARS_HOME="${DOCPARS_HOME:-$(cd "$(dirname "$0")/.." && pwd)}"
 export DOT_DOCOPT="${DOCPARS_HOME}/target/debug/docpars"
 dot::install_if_necessary
